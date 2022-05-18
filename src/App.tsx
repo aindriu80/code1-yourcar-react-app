87e11470d643e65bbcc67908ef4fdbef0636019e
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-05-18 08:20:36 +0100

    ViteJS React Typescript skeleton

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Yourcar - Rent the best quality cars with us</h1>
+    </div>
+  )
+}
+
+export default App
