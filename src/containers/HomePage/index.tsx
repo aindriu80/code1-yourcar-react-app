13709ff6b38a9352aa9e23505de88d588d811967
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-05-22 11:34:24 +0100

    Creating the landing page Cars top section

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -2,22 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 import tw from 'twin.macro'
 import { Navbar } from '../../components/navbar'
+import { TopSection } from './topSection'
 
 const PageContainer = styled.div`
-  ${tw`
-    flex
-    flex-col
-    w-full
-    h-full
-    items-center
-    overflow-x-hidden
-  `}
+  ${tw`flex flex-col items-center w-full h-full overflow-x-hidden `}
 `
 
 export function HomePage() {
   return (
     <PageContainer>
       <Navbar />
+      <TopSection />
     </PageContainer>
   )
 }
