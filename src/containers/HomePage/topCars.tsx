4aed8d1a7a965889995ce4b57f83414d50510e24
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-05-29 11:51:07 +0100

    Creating the TopCars component for showing list of cars

---
+++ src/containers/HomePage/topCars.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+import tw from 'twin.macro'
+import { Car } from '../../components/car'
+import { ICar } from '../../typings/car'
+
+const TopCarsContainer = styled.div`
+  ${tw`flex flex-col items-center justify-center w-full max-w-screen-lg pl-4 pr-4 mb-10 md:pl-0 md:pr-0`};
+`
+const Title = styled.h2`
+  ${tw`text-3xl font-black text-black lg:text-4xl`}
+`
+
+const CarsContainer = styled.div`
+  ${tw`flex flex-wrap justify-center w-full mt-7 md:mt-10`}
+`
+
+export function TopCars() {
+  const testCar: ICar = {
+    name: 'Audi S3 Car',
+    milage: '10k',
+    thumbnailSrc:
+      'https://cdn.jdpower.com/Models/640x480/2017-Audi-S3-PremiumPlus.jpg',
+    dailyPrice: 70,
+    monthlyPrice: 1600,
+    gearType: 'Auto',
+    gas: 'Petrol',
+  }
+
+  const testCar2: ICar = {
+    name: 'HONDA cITY 5 Seater Car',
+    milage: '20k',
+    thumbnailSrc:
+      'https://shinewiki.com/wp-content/uploads/2019/11/honda-city.jpg',
+    dailyPrice: 50,
+    monthlyPrice: 1500,
+    gearType: 'Auto',
+    gas: 'Petrol',
+  }
+  return (
+    <TopCarsContainer>
+      <Title>Explore Our Top Deals</Title>
+      <CarsContainer>
+        <Car {...testCar} />
+        <Car {...testCar} />
+        <Car {...testCar2} />
+      </CarsContainer>
+    </TopCarsContainer>
+  )
+}
