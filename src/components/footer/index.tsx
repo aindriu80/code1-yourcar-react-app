a98e0978f423d40fc218741a89bcc610091ce051
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-05-31 20:34:02 +0100

    Footer - Links

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,21 +4,95 @@ import tw from 'twin.macro'
 import { Logo } from '../logo'
 
 const FooterContainer = styled.div`
-  min-height: 20em;
-  background-color: rgb(31 42 76)
-    ${tw`flex flex-wrap min-w-full pt-1 bg-blue-900 pt-7`};
+  // min-height: 30em;
+  background-color: #1d2124;
+  ${tw`flex flex-col items-center justify-center min-w-full pt-10 md:pt-16`};
+`
+const InnerContainer = styled.div`
+  ${tw`flex flex-wrap w-full h-full max-w-screen-2xl`};
 `
 
 const AboutContainer = styled.div`
   ${tw`flex flex-col `}
 `
 
+const AboutText = styled.p`
+  ${tw`max-w-xs mt-2 text-sm font-light font-normal leading-5 text-white`}
+`
+
+const SectionContainer = styled.div`
+  ${tw`flex flex-col w-full pl-10 pr-10 mr-2 md:w-auto md:mr-16 md:pl-3 md:pr-3 mt-7 md:mt-0`};
+`
+
+const LinksList = styled.ul`
+  ${tw`flex flex-col list-none outline-none `}
+`
+
+const ListItem = styled.li`
+  ${tw`mb-3 `};
+
+  & > a {
+    ${tw`text-sm text-white transition-all hover:text-gray-200`};
+  }
+`
+
+const HeaderTitle = styled.h3`
+  ${tw`mb-3 text-base font-bold text-white `}
+`
+
 export function Footer() {
   return (
     <FooterContainer>
-      <AboutContainer>
-        <Logo color="white" />
-      </AboutContainer>
+      <InnerContainer>
+        <AboutContainer>
+          <Logo color="white" bgColor="dark" />
+          <AboutText>
+            Yourcar is a car renting and selling company located in many
+            countries across the world which has high quality cars and top rated
+            service.
+          </AboutText>
+        </AboutContainer>
+        <SectionContainer>
+          <HeaderTitle>Our Links</HeaderTitle>
+          <LinksList>
+            <ListItem>
+              <a href="#">Home</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">About Us</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Services</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Models</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Blog</a>
+            </ListItem>
+          </LinksList>
+        </SectionContainer>
+        <SectionContainer>
+          <HeaderTitle>Other Links</HeaderTitle>
+          <LinksList>
+            <ListItem>
+              <a href="#">FAQ</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Contact Us</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Support</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Privacy Policy</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Terms &amp; Conditions</a>
+            </ListItem>
+          </LinksList>
+        </SectionContainer>
+      </InnerContainer>
     </FooterContainer>
   )
 }
